e6157d96258410ac6b88d01b8180a90140ebea78
Peter Liu <sxpliu@gmail.com>
2020-10-03 20:03:44 +0800

    Modify for build go sm version

--- scripts/common/setup.sh
+++ scripts/common/setup.sh
@@ -32,28 +32,29 @@ export GOROOT="/opt/go"
 mkdir -p $GOPATH
 ARCH=`uname -m | sed 's|i686|386|' | sed 's|x86_64|amd64|'`
 BINTARGETS="x86_64 ppc64le s390x"
-GO_VER=1.13.12
+GO_VER=1.14.9
 
 # Install Golang binary if found in BINTARGETS
 if echo $BINTARGETS | grep -q `uname -m`; then
    cd /tmp
    wget --quiet --no-check-certificate https://storage.googleapis.com/golang/go${GO_VER}.linux-${ARCH}.tar.gz
    tar -xvf go${GO_VER}.linux-${ARCH}.tar.gz
-   mv go $GOROOT
-   chmod 775 $GOROOT
-# Otherwise, build Golang from source
-else
+#   mv go $GOROOT
+#   chmod 775 $GOROOT
+# # Otherwise, build Golang from source
+# else
    # Install Golang 1.6 binary as a bootstrap to compile the Golang GO_VER source
-   apt-get -y install golang-1.6
+#   apt-get -y install golang-1.13
 
    cd /tmp
-   wget --quiet --no-check-certificate https://storage.googleapis.com/golang/go${GO_VER}.src.tar.gz
-   tar -xzf go${GO_VER}.src.tar.gz -C /opt
+   #wget --quiet --no-check-certificate https://storage.googleapis.com/golang/go${GO_VER}.src.tar.gz
+   tar -xzf go${GO_VER}.tar.gz -C /opt
 
    cd $GOROOT/src
-   export GOROOT_BOOTSTRAP="/usr/lib/go-1.6"
+   export GOROOT_BOOTSTRAP="/tmp/go"
    ./make.bash
-   apt-get -y remove golang-1.6
+#   apt-get -y remove golang-1.13
+   
 fi
 
 PATH=$GOROOT/bin:$GOPATH/bin:$PATH
